f50dacd711af6b38b986af54424b7b18d1695781
minisbett <39670899+minisbett@users.noreply.github.com>
2025-03-24 13:21:04 +0100

    implement /scores endpoint

--- osu.NET/Endpoints/Scores.cs
+++ osu.NET/Endpoints/Scores.cs
@@ -1,8 +1,29 @@
-﻿namespace osu.NET;
+﻿using osu.NET.Enums;
+using osu.NET.Models.Scores;
+
+namespace osu.NET;
 
 public partial class OsuApiClient
 {
   // API docs: https://osu.ppy.sh/docs/index.html#scores
 
-  // TODOENDPOINT: https://osu.ppy.sh/docs/index.html#get-scores97 (pagination)
+  /// <summary>
+  /// Returns a bundle of the most recent scores across osu! and the cursor string for fetching newer scores.
+  /// <br/><br/>
+  /// If <paramref name="cursor"/> is not specified, the 1000 most recent scores from oldest to newest are returned.<br/>
+  /// New scores that arrived after the last request can be fetched by providing <paramref name="cursor"/>.
+  /// <br/><br/>
+  /// API docs:<br/>
+  /// <a href="https://osu.ppy.sh/docs/index.html#get-scores97"/>
+  /// </summary>
+  /// <param name="ruleset">Optional. The ruleset to return scores of.</param>
+  /// <param name="cursor">Optional. The cursor string for fetching newer scores.</param>
+  /// <param name="cancellationToken">Optional. The cancellation token for aborting the request.</param>
+  /// <returns>The bundle with scores.</returns>
+  public async Task<ApiResult<ScoresBundle>> GetScoresAsync(Ruleset? ruleset = null, string? cursor = null, CancellationToken? cancellationToken = null)
+    => await GetAsync<ScoresBundle>("scores", cancellationToken,
+    [
+      ("ruleset", ruleset),
+      ("cursor_string", cursor)
+    ]);
 }
---
+++ osu.NET/Models/Scores/ScoresBundle.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace osu.NET.Models.Scores;
+
+/// <summary>
+/// Represents a bundle containing scores and the cursor string for pagination.
+/// <br/><br/>
+/// API docs: <a href="https://osu.ppy.sh/docs/index.html#get-scores97"/> (response)<br/>
+/// Source: <a href="https://github.com/ppy/osu-web/blob/master/app/Http/Controllers/ScoresController.php"/>
+/// </summary>
+public class ScoresBundle
+{
+  /// <summary>
+  /// The cursor string for fetching further scores.
+  /// </summary>
+  [JsonProperty("cursor_string")]
+  public string Cursor { get; private set; } = default!;
+
+  /// <summary>
+  /// The scores.
+  /// </summary>
+  [JsonProperty("scores")]
+  public Score[] Scores { get; private set; } = default!;
+}
