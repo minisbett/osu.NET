dde8e6cd4d6b4b9e3a351e38f9bca998002b4418
minisbett <39670899+minisbett@users.noreply.github.com>
2026-01-02 14:54:12 +0100

    change score statistics to always be null by default

--- osu.NET/Models/Scores/ScoreStatistics.cs
+++ osu.NET/Models/Scores/ScoreStatistics.cs
@@ -3,50 +3,45 @@
 namespace osu.NET.Models.Scores;
 
 /// <summary>
-/// Represents the score statistics (hit judgements) of a score.<br/>
-/// Judgements that represent hits (eg. Large Tick Hit, Small Bonus) default to <see langword="null"/> if they are not present in the score.<br/>
-/// Judgements that represent misses or negative judgements (eg. Great, Miss, Large Tick Miss) default to 0 if they are not present in the score.
+/// Represents the score statistics (hit judgements) of a score. If a score does not contain a certain judgement type, it will be null.
 /// </summary>
 public class ScoreStatistics
 {
-  // TODOFUT: not necessarily happy with the way these statistics are handled, especially regarding their default values
-  //          this likely needs more thoughts into how this object would be used by people using the library
-
   /// <summary>
   /// The Perfect judgements of the score. This will be null if the score does not have any Perfect judgements.
   /// </summary>
   [JsonProperty("perfect")]
-  public int Perfect { get; private set; } = 0;
+  public int? Perfect { get; private set; }
 
   /// <summary>
-  /// The Great judgements of the score. This will be 0 if the score does not have any Great judgements.
+  /// The Great judgements of the score. This will be null if the score does not have any Great judgements.
   /// </summary>
   [JsonProperty("great")]
-  public int Great { get; private set; } = 0;
+  public int? Great { get; private set; }
 
   /// <summary>
   /// The Good judgements of the score. This will be 0 if the score does not have any Good judgements.
   /// </summary>
   [JsonProperty("good")]
-  public int Good { get; private set; } = 0;
+  public int? Good { get; private set; }
 
   /// <summary>
-  /// The Ok judgements of the score. This will be 0 if the score does not have any Ok judgements.
+  /// The Ok judgements of the score. This will be null if the score does not have any Ok judgements.
   /// </summary>
   [JsonProperty("ok")]
-  public int Ok { get; private set; } = 0;
+  public int? Ok { get; private set; }
 
   /// <summary>
-  /// The Meh judgements of the score. This will be 0 if the score does not have any Meh judgements.
+  /// The Meh judgements of the score. This will be null if the score does not have any Meh judgements.
   /// </summary>
   [JsonProperty("meh")]
-  public int Meh { get; private set; } = 0;
+  public int? Meh { get; private set; }
 
   /// <summary>
-  /// The Miss judgements of the score. This will be 0 if the score does not have any Miss judgements.
+  /// The Miss judgements of the score. This will be null if the score does not have any Miss judgements.
   /// </summary>
   [JsonProperty("miss")]
-  public int Miss { get; private set; } = 0;
+  public int? Miss { get; private set; }
 
   /// <summary>
   /// The Large Tick Hit judgements of the score. This will be null if the score does not have any Large Tick Hit judgements.
@@ -55,10 +50,10 @@ public class ScoreStatistics
   public int? LargeTickHit { get; private set; }
 
   /// <summary>
-  /// The Large Tick Miss judgements of the score. This will be 0 if the score does not have any Large Tick Miss judgements.
+  /// The Large Tick Miss judgements of the score. This will be null if the score does not have any Large Tick Miss judgements.
   /// </summary>
   [JsonProperty("large_tick_miss")]
-  public int LargeTickMiss { get; private set; } = 0;
+  public int? LargeTickMiss { get; private set; }
 
   /// <summary>
   /// The Large Bonus judgements of the score. This will be null if the score does not have any Large Bonus judgements.
@@ -67,16 +62,16 @@ public class ScoreStatistics
   public int? LargeBonus { get; private set; }
 
   /// <summary>
-  /// The Small Tick Hit judgements of the score. This will be 0 if the score does not have any Small Tick Hit judgements.
+  /// The Small Tick Hit judgements of the score. This will be null if the score does not have any Small Tick Hit judgements.
   /// </summary>
   [JsonProperty("small_tick_hit")]
-  public int SmallTickHit { get; private set; } = 0;
+  public int? SmallTickHit { get; private set; }
 
   /// <summary>
-  /// The Small Tick Miss judgements of the score. This will be 0 if the score does not have any Small Tick Miss judgements.
+  /// The Small Tick Miss judgements of the score. This will be null if the score does not have any Small Tick Miss judgements.
   /// </summary>
   [JsonProperty("small_tick_miss")]
-  public int SmallTickMiss { get; private set; } = 0;
+  public int? SmallTickMiss { get; private set; }
 
   /// <summary>
   /// The Small Bonus judgements of the score. This will be null if the score does not have any Small Bonus judgements.
@@ -91,10 +86,10 @@ public class ScoreStatistics
   public int? IgnoreHit { get; private set; }
 
   /// <summary>
-  /// The Ignore Miss judgements of the score. This will be 0 if the score does not have any Ignore Miss judgements.
+  /// The Ignore Miss judgements of the score. This will be null if the score does not have any Ignore Miss judgements.
   /// </summary>
   [JsonProperty("ignore_miss")]
-  public int IgnoreMiss { get; private set; } = 0;
+  public int? IgnoreMiss { get; private set; }
 
   /// <summary>
   /// The Legacy Combo Increase judgements of the score. This will be null if the score does not have any Legacy Combo Increase judgements.
